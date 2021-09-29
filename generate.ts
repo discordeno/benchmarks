11b9967eb35794961bbc6eb3c6abff5e5f8445a1
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-09-29 15:35:52 +0000

    get script finalized

--- generate.ts
+++ generate.ts
@@ -1,7 +1,7 @@
 import { Sabr, SabrTable } from "https://deno.land/x/sabr@1.1.4/mod.ts";
 import {
-  snakelize,
   startBot,
+  ws,
 } from "https://deno.land/x/discordeno@12.0.1/mod.ts";
 import { TOKEN } from "./configs.ts";
 import {
@@ -11,8 +11,6 @@ import {
   hideSnowflake,
   loopObject,
 } from "./utils.ts";
-
-const test: Record<string, any> = {};
 
 const sabr = new Sabr();
 
@@ -35,6 +33,8 @@ const FINISHED = new Set([
   "MESSAGE_REACTION_ADD",
 ]);
 
+let counter = 1;
+
 startBot({
   token: TOKEN,
   intents: [
@@ -50,163 +50,31 @@ startBot({
     "Guilds",
   ],
   eventHandlers: {
-    raw: async function (data) {
-      // DEVELOPMENT ONLY REMOVE WHEN DONE
-      if (data.t && FINISHED.has(data.t)) return;
-
-      if (!data.t) return console.log("NO T FOUND", data);
-
-      if (!test[data.t]) {
-        test[data.t] = data;
-        console.log(data);
-      }
-
-      const payload = data.d as any;
-      // let cleanPayload: Record<string, any> = {};
-
-      const numberreg = /^\d+$/;
-      const datereg =
-        /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/;
-
-      const cleanPayload = loopObject(payload, (value, key) => {
-        if (typeof value !== "string") return value;
-
-        // IF ITS A NUMBER MASK NUMBER
-        if (numberreg.test(value)) return hideSnowflake(value);
-        if (["icon", "banner", "splash", "avatar"].includes(key))
-          return hideHash(value);
-        // IF ITS DATE REGEX
-        if (Date.parse(value)) return hideDate();
-
-        if (!["username", "content", "hash", "session_id", "nick", "preferred_locale", '"fdvopm'].includes(key)) console.log(key, value);
-        return hideEUDText(value);
-      });
-
-      db.events.create(Date.now().toString(), cleanPayload);
-      // switch (data.t) {
-      //   case "READY": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       user: {
-      //         ...payload.user,
-      //         username: hideEUDText(payload.user.username),
-      //         id: hideSnowflake(payload.user.id),
-      //         discrimnator: hideSnowflake(payload.user.discriminator),
-      //         avatar: hideHash(payload.user.avatar),
-      //       },
-      //       guilds: payload.guilds.map((ug: any) => ({
-      //         ...ug,
-      //         id: hideSnowflake(ug.id),
-      //       })),
-      //       application: {
-      //         ...payload.application,
-      //         id: hideSnowflake(payload.application.id),
-      //       },
-      //     };
-      //     break;
-      //   }
-      //   case "INVITE_CREATE": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       inviter: {
-      //         username: hideEUDText(payload.inviter.username),
-      //         id: hideSnowflake(payload.inviter.id),
-      //         discriminator: hideSnowflake(payload.inviter.discriminator),
-      //         avatar: hideHash(payload.inviter.avatar),
-      //       },
-      //       guild_id: hideSnowflake(payload.guild_id),
-      //       created_at: hideDate(),
-      //       code: hideEUDText(payload.code),
-      //       channel_id: hideSnowflake(payload.channel_id),
-      //     };
-      //     break;
-      //   }
-      //   case "INVITE_DELETE": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       guild_id: hideSnowflake(payload.guild_id),
-      //       code: hideEUDText(payload.code),
-      //       channel_id: hideSnowflake(payload.channel_id),
-      //     };
-      //     break;
-      //   }
-      //   // @ts-ignore undocumented event
-      //   case "GUILD_JOIN_REQUEST_DELETE": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       user_id: hideSnowflake(payload.user_id),
-      //       guild_id: hideSnowflake(payload.guild_id),
-      //     };
-      //     break;
-      //   }
-      //   case "MESSAGE_REACTION_ADD": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       user_id: hideSnowflake(payload.user_id),
-      //       message_id: hideSnowflake(payload.message_id),
-      //       member: {
-      //         ...payload,
-      //         user: {
-      //           ...payload.member.user,
-      //           username: hideEUDText(payload.member.user.username),
-      //           id: hideSnowflake(payload.member.user.id),
-      //           discrimnator: hideSnowflake(payload.member.user.discriminator),
-      //           avatar: hideHash(payload.member.user.avatar),
-      //         },
-      //         roles: payload.member.roles.map((id: string) =>
-      //           hideSnowflake(id)
-      //         ),
-      //         joined_at: hideDate(),
-      //       },
-      //       emoji: {
-      //         ...payload.emoji,
-      //         name: hideEUDText(payload.emoji.name),
-      //         id: hideSnowflake(payload.emoji.id),
-      //       },
-      //       channel_id: hideSnowflake(payload.channel_id),
-      //       guild_id: hideSnowflake(payload.guild_id),
-      //     };
-      //     break;
-      //   }
-      //   case "GUILD_MEMBER_ADD": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       user: {
-      //         ...payload.user,
-      //         username: hideEUDText(payload.user.username),
-      //         id: hideSnowflake(payload.user.id),
-      //         discrimnator: hideSnowflake(payload.user.discriminator),
-      //         avatar: hideHash(payload.user.avatar),
-      //       },
-      //       roles: payload.roles.map((id: string) => hideSnowflake(id)),
-      //       joined_at: hideDate(),
-      //       premium_since: payload.premium_since
-      //         ? hideDate()
-      //         : payload.premium_since,
-      //       guild_id: hideSnowflake(payload.guild_id),
-      //       avatar: hideHash(payload.avatar)
-      //     };
-      //     break;
-      //   }
-      //   case "MESSAGE_CREATE": {
-      //     cleanPayload = {
-      //       ...payload,
-      //       id: hideSnowflake(payload.id),
-      //       embeds: payload.embeds.map(e => ({
-      //         ...e,
-      //         timestamp: e.timestamp ? hideDate() : undefined,
-      //         image: ,
-      //         description: hideEUDText(e.description),
-      //       }))
-      //     };
-      //     break;
-      //   }
-      // }
-
-      // make it snake case to be safe
-      const final = snakelize(cleanPayload);
-
-      // TODO: SAVE TO A JSON FILE
-    },
+    raw: async function (data) {},
   },
 });
+
+ws.log = function (type, data: any) {
+  if (type !== "RAW") return;
+
+  const payload = data as any;
+  const numberreg = /^\d+$/;
+
+  const cleanPayload = loopObject(payload, (value, key) => {
+    if (typeof value !== "string") return value;
+
+    // IF ITS A NUMBER MASK NUMBER
+    if (numberreg.test(value)) return hideSnowflake(value);
+    if (["icon", "banner", "splash", "avatar"].includes(key))
+      return hideHash(value);
+    // IF ITS DATE REGEX
+    if (Date.parse(value)) return hideDate();
+
+    return hideEUDText(value);
+  });
+
+  // if (data.t === "MESSAGE_CREATE") console.log("mc,", payload, cleanPayload);
+  console.log("Creating Event #", counter)
+  db.events.create(counter.toString(), cleanPayload);
+  counter++;
+};
--- utils.ts
+++ utils.ts
@@ -1,61 +1,104 @@
-const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const numbers = ["0", "1", "2", "3", "4","5","6","7","8","9"];
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 export function hideEUDText(text: string) {
-    return text ? text
-    .split("").map(character => {
-        if (character === " ") return character;
-        if (numbers.includes(character)) return hideSnowflake(character);
-        
-        const random = chooseRandom(alphabet)
-        return alphabet.includes(character) ? random : random.toLowerCase()
-    })
-    .join("") : text
+  return text
+    ? text
+        .split("")
+        .map((character) => {
+          if (character === " ") return character;
+          if (numbers.includes(character)) return hideSnowflake(character);
+
+          const random = chooseRandom(alphabet);
+          return alphabet.includes(character) ? random : random.toLowerCase();
+        })
+        .join("")
+    : text;
 }
 
 export function hideSnowflake(snowflake: string) {
-    return snowflake ? snowflake
-    .split("")
-    .map((_, index) => chooseRandom(index ? numbers : numbers.slice(1)))
-    .join("") : snowflake
+  return snowflake
+    ? snowflake
+        .split("")
+        .map((_, index) => chooseRandom(index ? numbers : numbers.slice(1)))
+        .join("")
+    : snowflake;
 }
 
 export function hideHash(hash: string | null) {
-    return hash ? `${hash.startsWith("a_") ? "a_" : ""}eae5905ad2d18d7c8deca20478b088b5` : hash;
+  return hash
+    ? `${hash.startsWith("a_") ? "a_" : ""}eae5905ad2d18d7c8deca20478b088b5`
+    : hash;
 }
 
 export function hideDate() {
-    return new Date().toISOString()
+  return new Date().toISOString();
 }
 
 export function chooseRandom<T>(array: T[]) {
-    return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
 
-export function loopObject<T = {}>(obj: {}, handler: (value: unknown, key: string) => unknown) {
-    let res: Record<string, unknown> | unknown[] = {};
-  
-    if (Array.isArray(obj)) {
-      res = [];
-  
-      for (const o of obj) {
-        if (typeof o === "object" && !Array.isArray(o) && o !== null) {
-          // A nested object
-          res.push(loopObject(o as {}, handler));
-        } else {
-          res.push(handler(o, "array"));
-        }
-      }
-    } else {
-      for (const [key, value] of Object.entries(obj)) {
-        if (typeof value === "object" && !Array.isArray(value) && value !== null && !(value instanceof Blob)) {
-          // A nested object
-          res[key] = loopObject(value as {}, handler);
-        } else {
-          res[key] = handler(value, key);
-        }
+export function loopObject<T = {}>(
+  obj: {},
+  handler: (value: unknown, key: string) => unknown
+) {
+  let res: Record<string, unknown> | unknown[] = {};
+
+  if (Array.isArray(obj)) {
+    res = [];
+
+    for (const o of obj) {
+      if (typeof o === "object" && !Array.isArray(o) && o !== null) {
+        // A nested object
+        res.push(loopObject(o as {}, handler));
+      } else {
+        res.push(handler(o, "array"));
       }
     }
-  
-    return res as T;
+  } else {
+    for (const [key, value] of Object.entries(obj)) {
+      if (
+        Array.isArray(value) ||
+        (typeof value === "object" &&
+          !Array.isArray(value) &&
+          value !== null &&
+          !(value instanceof Blob))
+      ) {
+        // A nested object
+        res[key] = loopObject(value as {}, handler);
+      } else {
+        res[key] = handler(value, key);
+      }
+    }
   }
+
+  return res as T;
+}
